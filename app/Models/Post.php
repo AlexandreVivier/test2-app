b6b7fdd61da1fad5501f2ffe56c8db39b10a5470
Krill <80386194+AlexandreVivier@users.noreply.github.com>
2022-12-24 17:26:43 +0100

    refactor(all)
    emploi des collections pour épurer le code
    
    ISSUE 14

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post 
 {
@@ -30,18 +31,38 @@ class Post
     // pour récupérer tous les posts dans resources/posts :
     public static function all()
     {
+        // On utilise collect (ramasse un array et le met dans une collection d'objets):
+        return collect(File::files(resource_path("posts")))
+         // -> map est la fonction array_map de la collection :
+        ->map(fn ($file) => YamlFrontMatter::parseFile($file))
+        ->map(fn ($document) => new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug,
+        ));
+        /* Ancienne version :       
             //on extrait les files via facades :
             $files = File::files(resource_path("posts"));
 
             //array_map() fonctionne comme une loop, mais return un nouvel array
             // 1er argu :  la fonction à appliquer à 2nd argu : le tableau que tu passes dans le 1er
             return array_map(fn($file) => $file->getContents(), $files);
+            */
     }
 
 
     // methode find() pour trouver selon le slug
     public static function find($slug)
     {
+        //Nouvelle méthode, avec collection (episode 12):
+
+        $post = static::all();
+        // $post est une collection désormais :
+
+        return $post->firstWhere('slug', $slug);
+        /* Ancienne méthode, sans collection :
         // Définir le chemin d'accès contenu par le slug pour le passer dans la vue ($path)
         // vérifier que le chemin/slug existe sinon rediriger :
         if (! file_exists($path = resource_path("/posts/{$slug}.html"))) {
@@ -53,6 +74,6 @@ class Post
         
         // stocker dans le cache 20min pour éviter de surcharger la méthode :
         return cache()->remember("posts.{$slug}", now()->addMinutes(20), fn () => file_get_contents($path));
-
+        */
     }
 }
