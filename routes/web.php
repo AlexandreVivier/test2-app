b0e57cb020903729918044ae8c2d6a1f5b6bae23
Krill <80386194+AlexandreVivier@users.noreply.github.com>
2023-01-12 19:08:55 +0100

    feature(categories)
    Eloquent relationships
    
    ISSUE 25

--- routes/web.php
+++ routes/web.php
@@ -190,7 +190,7 @@ Route::get('post', function() {
 
 // Afficher les catégories :
 
-Route::get('categories/{category}', function (Category $category) {
+Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'posts' => $category->posts,
     ]);
