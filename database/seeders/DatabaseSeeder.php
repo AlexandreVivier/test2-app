0b6038b62d52fb18c1b247ac30e82272ea53cb9f
Krill <80386194+AlexandreVivier@users.noreply.github.com>
2023-01-12 22:33:45 +0100

    feature(users)
    Ajout des users WIP
    
    ISSUE 27

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(1)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        /* \App\Models\User::factory()->create([
+             'name' => 'Test User',
+             'email' => 'test@example.com',
+         ]);
+        */
+
+       /* \App\Models\User::create([
+            'name'=> 'Personal',
+            'slug'=>'personal'
+         ]);
+*/
+         \App\Models\Category::create([
+            'name'=> 'Personal',
+            'slug'=>'personal'
+         ]);
+
+         \App\Models\Category::create([
+            'name'=> 'Family',
+            'slug'=>'family'
+         ]);
+
+         \App\Models\Category::create([
+            'name'=> 'Work',
+            'slug'=>'work'
+         ]);
     }
 }
