f80b7fe8cc8afe548b3384c2ca7695b4504b614d
Krill <80386194+AlexandreVivier@users.noreply.github.com>
2023-01-13 15:48:02 +0100

    feature(factory)
    Facotries.
    
    ISSUE 29

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,14 @@ class DatabaseSeeder extends Seeder
          \App\Models\Category::truncate();
          \App\Models\Post::truncate();
 
-         //\App\Models\User::factory(1)->create();
+         $user = \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+         \App\Models\Post::factory(5)->create([
+            'user_id' => $user->id
+         ]);
 
         /* \App\Models\User::factory()->create([
              'name' => 'Test User',
@@ -28,7 +35,7 @@ class DatabaseSeeder extends Seeder
          ]);
         */
 
-        $user = \App\Models\User::create([
+        /*$user = \App\Models\User::create([
             'name'=> 'Auteur',
             'email'=>'personal@internet.com',
             'password'=>'topSecret'
@@ -75,5 +82,6 @@ class DatabaseSeeder extends Seeder
             'excerpt' => 'Du travail, encore du travail, lorem ipsum...',
             'body' => ' <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed urna purus. Mauris hendrerit sodales purus, a aliquet purus pharetra et. Suspendisse interdum tristique purus sit amet tincidunt. Ut semper convallis egestas. In id massa et lectus efficitur ullamcorper et at velit. Ut pharetra scelerisque tincidunt. Phasellus lobortis id ex at lobortis. Praesent ullamcorper finibus finibus. Fusce in placerat lacus. Duis gravida nisi id nisi viverra laoreet. Fusce commodo ultrices enim eget lacinia. In eu neque orci. Aliquam placerat tristique rhoncus. Maecenas non lorem eu felis vulputate bibendum. Aenean lacinia commodo rutrum. Donec non feugiat est, non feugiat lectus. Maecenas maximus luctus mauris et cursus. Sed leo magna, vulputate at libero in, luctus maximus eros. Aenean sed erat eget justo posuere venenatis ac a nisi. Vestibulum et neque mattis, vehicula diam in, vestibulum orci. Curabitur lorem augue, interdum a lacus sed, fringilla tincidunt diam. Mauris sodales aliquet diam, vel venenatis leo feugiat nec. </p>',
          ]);
+         */
     }
 }
