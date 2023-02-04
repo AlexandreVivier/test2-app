9f512c5467111c1f76a189668a27c6b0e2f081a9
Krill <80386194+AlexandreVivier@users.noreply.github.com>
2023-02-04 17:02:58 +0100

    feature(dropdown-item)
    Extract dropdown-item en component
    
    ISSUE 42

---
+++ resources/views/components/dropdown.blade.php
@@ -0,0 +1,16 @@
+@props(['trigger'])
+{{-- trigger --}}
+<div x-data="{ show:false }" @click.away="show = false">
+                        <div @click="show = ! show">
+                            {{ $trigger }}
+                        </div>
+
+                        {{-- lINKS --}}
+                        <div x-show="show" 
+                        class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50" 
+                        style="display:none;"
+                        >
+                        {{ $slot }}
+                            </div>
+                    </div>
+                    
